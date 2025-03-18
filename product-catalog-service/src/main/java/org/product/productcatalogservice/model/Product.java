57315f340d5702ea4ece1c7d67aeba14d25e8bc5
rama0534 <sadamramanayudu@gmail.com>
2025-03-17 20:33:34 -0500

    lombok config fixed

--- product-catalog-service/src/main/java/org/product/productcatalogservice/model/Product.java
+++ product-catalog-service/src/main/java/org/product/productcatalogservice/model/Product.java
@@ -3,55 +3,16 @@ package org.product.productcatalogservice.model;
 import jakarta.persistence.*;
 import lombok.*;
 
+
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Getter @Setter
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     public int product_id;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getProduct_id() {
-        return product_id;
-    }
-
-    public void setProduct_id(int product_id) {
-        this.product_id = product_id;
-    }
-
-    public String getBrand() {
-        return brand;
-    }
-
-    public void setBrand(String brand) {
-        this.brand = brand;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
 
     public String name;
     public String brand;
